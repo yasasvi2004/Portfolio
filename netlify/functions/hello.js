37b984e4c545a77cfdb026b1ccabeba696370787
Yasasvi Krishna <pandu.krishna04@gmail.com>
2023-06-30 18:22:12 +0530

    r

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,11 +1,11 @@
 
 
 exports.handler = async () => {
-  const apiKey = process.env.API_KEY;
-  console.log('API_KEY:', apiKey);
+  const apiKey = process.env.HELLO;
+  console.log('HELLO:', apiKey);
 
   return {
     statusCode: 200,
-    body: `Environment variable API_KEY value: ${apiKey}`,
+    body: `Environment variable HELLO value: ${apiKey}`,
   };
 };
