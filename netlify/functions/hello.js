bf2df639679c6005f1473fbf5fb9a657b560c35d
Yasasvi Krishna <pandu.krishna04@gmail.com>
2023-06-30 17:42:06 +0530

    saved to github

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -4,3 +4,11 @@ exports.handler = async () => {
       body: 'hello world!',
     };
   };
+  exports.handler = async () => {
+    const mySecret = process.env.MY_SECRET;
+    return {
+      statusCode: 200,
+      body: `hello world! I have a ${mySecret}`,
+    };
+  };
+  
