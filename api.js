1b4f73d27ffd8b46e1ef6cbb0f2368492307d2db
Yasasvi Krishna <pandu.krishna04@gmail.com>
2023-06-30 16:24:52 +0530

    saved to github

---
+++ api.js
@@ -0,0 +1,10 @@
+fetch('http://localhost:8888/.netlify/functions/hello')
+  .then(response => response.json())
+  .then(data => {
+    // Handle the response data
+    console.log(data);
+  })
+  .catch(error => {
+    // Handle any errors
+    console.error(error);
+  });
